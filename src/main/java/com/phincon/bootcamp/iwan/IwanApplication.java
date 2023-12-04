a4a991d4761481f7e7e326c530823471f121ad31
112tenday <iwangunawantondang@gmail.coml>
2023-12-04 13:22:22 +0700

    first commit

---
+++ src/main/java/com/phincon/bootcamp/iwan/IwanApplication.java
@@ -0,0 +1,13 @@
+package com.phincon.bootcamp.iwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwanApplication.class, args);
+	}
+
+}
