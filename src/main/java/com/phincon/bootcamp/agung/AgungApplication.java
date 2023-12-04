8c314ea0c7326a31ef09d09cecd8cebf39bcfa91
agngfbrnsyh <agungfbrnsyh98@gmail.com>
2023-12-04 11:35:28 +0700

    first commit

---
+++ src/main/java/com/phincon/bootcamp/agung/AgungApplication.java
@@ -0,0 +1,13 @@
+package com.phincon.bootcamp.agung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phincon/bootcamp/agung/AgungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phincon.bootcamp.agung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
