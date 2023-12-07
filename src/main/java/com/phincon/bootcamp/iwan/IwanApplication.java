d8e9c0901c57846b51ed713b7cf87463d5464777
112tenday <iwangunawantondang@gmail.com>
2023-12-07 14:21:59 +0700

    fisrt commit

---
+++ src/main/java/com/phincon/bootcamp/iwan/IwanApplication.java
@@ -0,0 +1,36 @@
+package com.phincon.bootcamp.iwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwanApplication.class, args);
+	}
+
+	// @Override
+	// public void run(String... args) throws Exception {
+	// for (int i = 0; i < 10; i++) {
+	// Account account = new Account();
+	// account.setId(i);
+	// System.out.println("id :" + account.getId());
+	// }
+
+	// boolean max = true;
+	// int d = 0;
+	// while (max) {
+	// d++;
+	// Account account = new Account();
+	// account.setId(d);
+	// System.out.println("while id :" + account.getId());
+
+	// if (d == 5) {
+	// max = false;
+	// }
+	// }
+
+	// }
+
+}
