4f4421fb4aff72a65df61c2f631e621781168e2a
112tenday <iwangunawantondang@gmail.com>
2023-12-14 11:57:17 +0700

    ada salah dikit tadi di schduller

--- src/main/java/scheduller/Scheduller.java
+++ src/main/java/scheduller/Scheduller.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 import com.phincon.bootcamp.iwan.repository.AccountRepository;
+
 @RestController
 public class Scheduller {
     Logger logger = LoggerFactory.getLogger(Scheduller.class);
-    @Autowired     AccountRepository accountRepository;
+    @Autowired
+    AccountRepository accountRepository;
+
     @Scheduled(cron = "/5 * * * ?")
     public void cronJobSch() {
         logger.info("jalan");
-        accountRepository.getAccountById("1");     }  }
+        accountRepository.getAccountById("1");
+    }
+}
